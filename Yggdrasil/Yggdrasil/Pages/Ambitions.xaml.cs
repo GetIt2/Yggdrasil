91fbf37efe69b35067e64ee4cdedaec88e6fb07d
Kristoffer Kvamme <33118819+KrisKvam@users.noreply.github.com>
2018-06-28 09:34:38 +0200

    made ambitions c#

--- Yggdrasil/Yggdrasil/Pages/Ambitions.xaml.cs
+++ Yggdrasil/Yggdrasil/Pages/Ambitions.xaml.cs
@@ -15,6 +15,68 @@ namespace Yggdrasil.Pages
 		public Ambitions ()
 		{
 			InitializeComponent ();
+
+		    ViewAmbitionMainPage();
 		}
+
+	    private void ViewAmbitionMainPage()
+	    {
+            var header = new Label()
+            {
+                Text = "Ambitions",
+                FontSize = 50,
+                VerticalOptions = LayoutOptions.Start,
+                HorizontalOptions = LayoutOptions.CenterAndExpand,
+                Margin = 60
+            };
+            var addButton = new Button()
+            {
+                Text = "Add new",
+                HorizontalOptions = LayoutOptions.Start,
+                Margin = 10
+            };
+            var scrollingAmbitions = new ScrollView()
+            {
+                BackgroundColor = Color.DarkGray,
+                VerticalOptions = LayoutOptions.FillAndExpand,
+                Margin = 10
+            };
+            Content = new StackLayout()
+            {
+                Children =
+                {
+                    header,
+                    addButton,
+                    scrollingAmbitions
+                }
+            };
+	        scrollingAmbitions.Content = GetAmbitions();
+	    }
+
+	    private StackLayout GetAmbitions()
+	    {
+	        var ambitions = new StackLayout();
+	        var numberOfAmbitions = 10;
+
+	        for (int i = 0; i < numberOfAmbitions; i++)
+	        {
+                var view = new ContentView()
+                {
+	                BackgroundColor = i % 2 == 0 ? Color.Gray : Color.DimGray,
+	                HorizontalOptions = LayoutOptions.FillAndExpand,
+                    Padding = 10,
+                    Margin = 2
+                };
+	            var ambition = new Label
+	            {
+	                Text = $"Ambition {i}",
+                    TextColor = Color.White
+	            };
+	            view.Content = ambition;
+	            ambitions.Children.Add(view);
+	        }
+
+	        return ambitions;
+	    }
 	}
 }
